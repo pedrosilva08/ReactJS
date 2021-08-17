a59c7f03e384b819aa5e6728db3a8c7f777f9514
Pedro <pedromello172@gmail.com>
2021-08-17 09:42:40 -0300

    Aprendendo sobre o funcionamento do JSX

--- react1/src/App.js
+++ react1/src/App.js
@@ -1,5 +1,5 @@
 //todo elemento é uum componente
-import react from 'react';
+import React from 'react';
 //apresentando classe como componente
 /*
 class App extends React.Component
@@ -21,9 +21,61 @@ let App = () =>
 {
   return <h1>Testando 6,7,8</h1>
 }
-*/
+
 //forma mais curta
 let App = () => <h1>Testando 10,11,12</h1>
 
 //exportar o app(class)
 export default App
+*/
+
+
+///////////// aula 3 de react/////////////////////
+//JSX =! html
+/*
+function App()
+{
+  //no JSx só pode retornar um elemento
+  let nome = 'Pedro';
+  let idade = 28;
+  return <>
+            <div>
+              Meu nome é: {nome} e eu tenho {idade} anos
+            </div>
+          </>
+}
+*/
+///////////////////////////segunda forma de fazer////////////////
+/*
+function formatarNome(usuario)
+{
+  return usuario.nome+' '+usuario.sobrenome;
+}
+
+function App()
+{
+  let usuario = 
+  {
+    nome: 'Pedro',
+    sobrenome: 'Silva'
+  }
+
+  return <>
+          <div>Meu nome é: {formatarNome(usuario)}</div>
+         </>
+}
+*/
+////teceira forma de fazer///////////////////////////
+function formatarNome(usuario)
+{
+  return usuario.nome+' '+usuario.sobrenome;
+}
+function App()
+{
+  let imagem = 'http://www.google.com.br/google.jpg';
+
+  return <>
+          <img src= {imagem}/>
+         </>
+}
+export default App;
