db8506bfc1d2ef5ef07d8fbf4884daa7c33cfb57
Pedro <pedromello172@gmail.com>
2021-09-13 21:28:14 -0300

    Aplicando Redux Em várias telas

--- react1/src/pages/login.js
+++ react1/src/pages/login.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom'
+import { connect } from "react-redux";
 
 
-function Login() {
+function Login(props) {
+    const handleFelipe = () => {
+        props.setName("Felipe")
+    }
     return(
         <div>
             <h4>Pagina Login</h4>
             
+            O nome é: {props.name}
+            <br/><br/>
+            <button onClick={handleFelipe}>Trocar nome para Felipe</button>
         </div>
     )
 }
-
-export default Login;
+const mapStateToProps = (state)=>{
+    return {
+        name: state.usuario.name
+    }
+}
+const mapDispatchToProps = (dispatch) => {
+    return{
+        setName: (newName) => dispatch({
+            type:'SET_NAME',
+            payload: { name:newName }//usado para mandar informações
+        }),
+    }
+}
+export default connect(mapStateToProps,mapDispatchToProps) (Login);
 
 
 
